820faa3c163cd2164153c6704937e5d1795ceb35
adian juan <juanrhamadhan@gmail.com>
2025-01-20 11:30:22 +0700

    Initial commit, spring init

---
+++ src/main/java/com/kkn/be_master/BeMasterApplication.java
@@ -0,0 +1,13 @@
+package com.kkn.be_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkn/be_master/BeMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkn.be_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
