3c6280e1a852e1f55dc1c6d19a10eb7d1995f90d
adian juan <juanrhamadhan@gmail.com>
2025-01-25 08:37:58 +0700

    Update test security

--- src/main/java/com/balia/be/web/rest/TestController.java
+++ src/main/java/com/balia/be/web/rest/TestController.java
@@ -1,19 +1,41 @@
 package com.balia.be.web.rest;
 
+import com.balia.be.config.security.SecurityUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
+    private final Logger log = LogManager.getLogger(TestController.class);
+    
     @GetMapping("/aja")
     public ResponseEntity<String> showMessage(){
         System.out.println(new Date() + "Test aja");
         return ResponseEntity.ok("test executed");
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String userAccess() {
+        log.info("Current login : {}",(SecurityUtils.getCurrentUserLogin()));
+        return "User Content.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        log.info("Current login admin : {}",(SecurityUtils.getCurrentUserLogin()));
+        return "Admin Board.";
     }
 }
